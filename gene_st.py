02cfcb163a4394ec32e03ed23cdbdbb4b0a51e03
jesusiv24921 <147682312+jesusiv24921@users.noreply.github.com>
2023-10-12 01:33:41 -0500

    Update gene_st.py

--- gene_st.py
+++ gene_st.py
@@ -32,7 +32,7 @@ generate_txt_button = st.button("Generate Text File")
 if file is not None:
     df = pd.read_csv(file, sep=";")
     df = df.dropna()
-    
+
     nuevo_tope = int(df['Tope'].min() - 10)
     nueva_base = int(df['Base'].max() + 10)
 
@@ -44,22 +44,15 @@ if file is not None:
     st.write("Updated DATE:", nueva_fecha)
 
     nuevos_valores = list(range(nuevo_tope, nueva_base + 1))
-    df_new = pd.DataFrame({'Depth': nuevos_valores})
+    df_new = pd.DataFrame({'Depth': nuevos_valles})
     df_new["Z1"] = df_new['Depth'].apply(lambda x: df.loc[(x >= df['Tope']) & (x <= df['Base']), '14/4/2023'].values[0] if any(((x >= df['Tope']) & (x <= df['Base']))) else 0.00)
 
     df_new['Z2'] = [0.00 for _ in range(len(df_new))]
     df_new['Z3'] = df_new["Z1"]
     df_new['Z4'] = [0.00 for _ in range(len(df_new))]
 
-    df_new['Z2'] = (df_new['Z2'])
-    df_new['Z3'] = df_new["Z1"]
-    df_new['Z4'] = [0 for _ in range(len(df_new))]
-
     df_new = df_new.astype(str)
-
     df_new = df_new.apply(lambda x: x.str.replace('.', ','))
-
-    st.dataframe(df_new)
 
     if generate_txt_button:
         # Procesa el archivo base_text directamente
@@ -73,34 +66,27 @@ if file is not None:
                     lines[i] = f'UWI. {nuevo_uwi}:UNIQUE WELL ID'
                 elif "DATE." in linea:
                     lines[i] = f'DATE. {nueva_fecha}:DATE'
-        
-            indice_inicio_seccion=None
 
-        for i, linea in enumerate(lines):
-            if "~A" in linea:
-                indice_inicio_seccion=i
-                break
-            #Remplazar las lineas actualizadas con el Dataframe
+            # Encuentra la línea que contiene "~A" para reemplazar la sección adecuada
+            indice_inicio_seccion = None
 
-        if indice_inicio_seccion is not None:
-            df_lines=df_new.apply(lambda row:'\t'.join(row.astype(str)),axis=1).to_list()
-            # df_lines=['\n'.join(df_lines[i:i+4]) for i in range(0,len(df_lines),4)]
-            # df_lines=df_new.to_string(header=False, index=False).split('\n')[1:]
-            lines[indice_inicio_seccion+1:indice_inicio_seccion+1+len(df_lines)]=df_lines
-        output.write("\n".join(lines).encode())
-        output.seek(0)
-        
-        st.success("nuevo.txt generated!")
+            for i, linea in enumerate(lines):
+                if "~A" in linea:
+                    indice_inicio_seccion = i
+                    break
 
-        st.subheader("Download nuevo.txt")
-        st.write("Click the button")
-        st.download_button(
-            label="Download",
-            data="\n".join(lines),
-            key="Download",
-            file_name="nuevo.txt",
-        )
+            # Remplaza las líneas actualizadas con el DataFrame
+            if indice_inicio_seccion is not None:
+                df_lines = df_new.apply(lambda row: '\t'.join(row.astype(str)), axis=1).to_list()
+                lines[indice_inicio_seccion + 1:indice_inicio_seccion + 1 + len(df_lines)] = df_lines
 
+            st.success("nuevo.txt generated!")
 
-
-
+            st.subheader("Download nuevo.txt")
+            st.write("Click the button")
+            st.download_button(
+                label="Download",
+                data="\n".join(lines),
+                key="Download",
+                file_name="nuevo.txt",
+            )
