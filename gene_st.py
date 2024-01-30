19174d5211fa1ed4590048509ebd917c375c6b9f
jesusiv24921 <147682312+jesusiv24921@users.noreply.github.com>
2024-01-30 16:03:59 -0500

    Update gene_st.py

--- gene_st.py
+++ gene_st.py
@@ -51,9 +51,11 @@ generate_txt_button = st.button("Generate Text File")
 
 
 if file is not None:
-    # delimitador= detectar_delimitador(file)
-    # st.write(delimitador)
-    df = pd.read_csv(file, sep=',', header=None)
+    df_1 = pd.read_csv(file, sep=',', header=None)
+    if len(df_1.columns)=1:
+        df = pd.read_csv(file, sep=';', header=None)
+    else:
+        df = pd.read_csv(file, sep=',', header=None)
     
     st.write(df)
     st.write(len(df.columns))
