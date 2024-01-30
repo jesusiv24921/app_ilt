7d2850e3f32e6218251854942339f574471087b2
jesusiv24921 <147682312+jesusiv24921@users.noreply.github.com>
2024-01-30 15:55:13 -0500

    Update gene_st.py

--- gene_st.py
+++ gene_st.py
@@ -51,8 +51,8 @@ generate_txt_button = st.button("Generate Text File")
 
 
 if file is not None:
-    # delimitador= detectar_delimitador(file)
-    df = pd.read_csv(file, sep=";", header=None)
+    delimitador= detectar_delimitador(file)
+    df = pd.read_csv(file, sep=delimitador, header=None)
     
     st.write(df)
     df_=df.iloc[0:2,0:2]
