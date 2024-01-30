a9c075b7b42d6b1d93a9a1decda0b508bfcc23b5
jesusiv24921 <147682312+jesusiv24921@users.noreply.github.com>
2024-01-30 16:44:38 -0500

    Update gene_st.py

--- gene_st.py
+++ gene_st.py
@@ -48,11 +48,13 @@ generate_txt_button = st.button("Generate Text File")
 
 if file is not None:
     df_1 = pd.read_csv(file, sep=';', header=None)
-    st.write(len(df_1.columns))
-    if len(df_1.columns)==6:
-        df = pd.read_csv(file_1, sep=';', header=None)
+    # st.write(len(df_1.columns))
+    if len(df_1.columns)!=1:
+        df = pd.read_csv(file_1, sep=',', header=None)
+        st.write(len(df.columns))
     else:
         df = pd.read_csv(file_1, sep=';', header=None)
+        st.write(len(df.columns))
         
     st.write(df)
     st.write(len(df.columns))
