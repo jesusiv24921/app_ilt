384707b3244adc9b4cb182efbbb6b8576a147b6e
jesusiv24921 <147682312+jesusiv24921@users.noreply.github.com>
2024-03-19 07:13:59 -0500

    Update gene_st.py

--- gene_st.py
+++ gene_st.py
@@ -51,7 +51,7 @@ if file is not None:
     df = df.drop(0).reset_index(drop=True)
     nombre_pozo=df_.iloc[0,1]
     fecha_registro=df_.iloc[1,1]
-    fecha_registro = pd.to_datetime(fecha_registro)
+    fecha_registro = pd.to_datetime(fecha_registro, format=('%d/%m/%Y'))
     fecha_registro_=fecha_registro.strftime('%d/%m/%Y')
     nuevo_well = st.sidebar.text_input("Name_1", nombre_pozo, disabled=True)
     nuevo_uwi = st.sidebar.text_input("Name_2", nombre_pozo, disabled=True)
